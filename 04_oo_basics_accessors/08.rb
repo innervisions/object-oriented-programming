890f5e1fc875d0a40b6b776b47dc350dedc2dba6
Raymond Saade <raymond.saade@gmail.com>
2020-12-26 21:30:34 -0500

    completed accessors

---
+++ 04_oo_basics_accessors/08.rb
@@ -0,0 +1,14 @@
+# 08 - Avoid Mutations
+class Person
+  def name
+    @name.dup
+  end
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+person1 = Person.new('James')
+person1.name.reverse!
+puts person1.name
