98e023695d4348d301c50605873921a22e1e8131
Raymond Saade <raymond.saade@gmail.com>
2021-01-02 22:36:35 -0500

    completed oo basics inheritance

---
+++ 03_oo_basics_inheritance/09.rb
@@ -0,0 +1,24 @@
+# 09 - Method Lookup (Part 3)
+module Flyable
+  def fly
+    "I'm flying!"
+  end
+end
+
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+  include Flyable
+end
+
+bird1 = Bird.new('Red')
+bird1.color # Will check Bird, then Flyable, then Animal
---
+++ 03_oo_basics_inheritance/10.rb
@@ -0,0 +1,11 @@
+# 10 - Transportation
+module Transportation
+  class Vehicle
+  end
+
+  class Car < Vehicle
+  end
+
+  class Truck < Vehicle
+  end
+end
